b5e1474544faa1242e197cccd9e8fd7714e09233
Fernando Saldaña <fernando_s_98@hotmail.com>
2022-06-07 17:49:34 -0500

    Update schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -16,25 +16,3 @@ CREATE TABLE medical_histories (
     patient_id int REFERENCES patients(id),
     status varchar(50),
 );
-
-CREATE TABLE invoices (
-    id serial PRIMARY KEY,
-    total_amount decimal,
-    generated_at timestamp,
-    payed_at timestamp,
-    medical_history_id REFERENCES medical_histories(id)
-);
-
-CREATE TABLE invoice_items (
-    unit_price decimal,
-    quantity int,
-    total_price decimal,
-    invoice_id int REFERENCES invoices(id),
-    treatment_id int REFERENCES treatments(id)
-);
-
-CREATE TABLE m_histories_treatments (
-    id serial PRIMARY KEY,
-    medical_history_id int REFERENCES medical_histories(id),
-    treatment_id int REFERENCES treatments(id)
-);
